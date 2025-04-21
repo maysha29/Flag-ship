86a42c009fffccda17f113359bd32ca93cecd182
maysha29 <mayshatabassum29@gmail.com>
2025-04-21 16:00:29 +0600

    Footer part added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+            <footer className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4">
   <aside>
     <p>Copyright © {new Date().getFullYear()} - All right reserved by FlagShipFaceOff Ltd</p>
   </aside>

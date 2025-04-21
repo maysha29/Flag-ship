3d3b9dc4bc56789f87c0974be7f5ea18febf42ff
maysha29 <mayshatabassum29@gmail.com>
2025-04-21 10:50:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-7xl text-sky-500'>good looking</h1>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    Component: App,
+  },
+  {
+    path:'/about',
+    element: <p>This is an about page</p>
+  }
+])
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
